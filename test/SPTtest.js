82e3a6d92047697da07b80afb9919e004165eaa6
Stepan Poperechnyi <poperechnyi1@gmail.com>
2017-11-10 16:49:50 +0200

    add test for check on ERC20 compatibility

--- test/SPTtest.js
+++ test/SPTtest.js
@@ -3,8 +3,33 @@ var SPT = artifacts.require("StepanToken2");
 
 contract("Testing ERC20 Contract", function(){
 
-    
-    it("Deploy contract", function(){
-        assert(TestContract.deployed());
+    //check deploy contract
+    it("Deploy contract whit two parameters", function(){
+        return SPT.deployed('0xC0b4ec83028307053Fbe8d00ba4372384fe4b52B','0x959FD7Ef9089B7142B6B908Dc3A8af7Aa8ff0FA1');
+    });
+
+    //check on ERC20 compatiblity
+    it("Current token compitibility with ERC20", function(){
+        return  SPT.deployed('0xC0b4ec83028307053Fbe8d00ba4372384fe4b52B',
+        '0x959FD7Ef9089B7142B6B908Dc3A8af7Aa8ff0FA1').then(
+            function(instance){
+                // console.log(instance);
+                assert(instance.totalSupply && instance.balanceOf && instance.transfer && instance.transferFrom && instance.approve && instance.allowance);
+            }
+        );
+    });
+
+    it("Check totalSupply()", function(){
+        return SPT.deployed('0xC0b4ec83028307053Fbe8d00ba4372384fe4b52B',
+        '0x959FD7Ef9089B7142B6B908Dc3A8af7Aa8ff0FA1').then(
+            function(instance){
+                return instance.totalSupply.call();
+            }
+        ).then(function(result){
+            assert.equal(result, 100000000000000);
+        })
     })
+
+    
+   
 })
